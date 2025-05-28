611cf0287dc48dba39e98be48f4474d3dc130ff9
QuivyGM <asimov@hanyang.ac.kr>
2025-05-28 17:25:57 +0900

    updat

--- assignments/inventory/inventory.py
+++ assignments/inventory/inventory.py
@@ -20,11 +20,11 @@ def quickSort(arr, low, high):
 if __name__ == "__main__":
 
     # save inventory id and stack size
-    temp_inventory = []
-    inventory_specs = {}
-    inventory_data = {}
-    inventory_col = 0
-    inventory_size = 0
+    temp_inventory = [] 
+    inventory_specs = {}    # data base for max items in cell
+    inventory_data = {} # data base for total number of items
+    inventory_col = 0   # how long single line is
+    inventory_size = 0  # how many cells are in input (preserve for output)
 
 
 
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         key, value = set.split(",")
         temp_inventory.append((int(key), int(value)))
 
-    # Apply quickSort
+    # Apply quickSort to dict
     inventory_size = len(temp_inventory)
     quickSort(temp_inventory, 0, inventory_size - 1)
     
@@ -49,52 +49,76 @@ if __name__ == "__main__":
     # print("-"*20)
     # print(inventory_data)
     
+    
+    inventory_size = 0
     # rest of input
     while True:
         try:
+            
             line = input().strip()
-            if inventory_col == 0:
-                inventory_col = len(line.split(","))
 
-
+            # if no more lines then break
             if not line:
                 break
-            # if item is- then skip
 
-            for item in line.split("),"):
+            # yes lines
 
+            # update how many cells in single line
+            if inventory_col == 0:
+                inventory_col = len(line.split(","))
+            # update inventory size to reflect how many cells are in input (excluding specs)
+            inventory_size += inventory_col
+            
+            # strip line
+            for item in line.split(","):
+                # strip to per item
                 item = item.strip()
+                print(item)
 
-                if item == "-":
-                    continue
+                # # item is - == empty cell, move onto next
+                # if item == "-":
+                #     continue
 
-                # else if item is (a, b) then save to inventory_data
-                else:
-                    item = item.strip("()")
-                    item_id, item_count = map(int, item.split(","))
-                    inventory_data[item_id] += item_count
+                # # else if item is (a, b) then save to inventory_data key:a value += b
+                # else:
+                #     item = item.strip("()")
+                #     item_id, item_count = map(int, item.split(","))
+                #     inventory_data[item_id] += item_count
             
         except EOFError:
             break
 
+    print(inventory_size)
+    print(inventory_data)
+
     # print inventory
-    #for range in inventory_size:
+
+    # list of item id
+    id_list = [item_id for item_id, _ in temp_inventory]
+
+    #for range in inventory_size (run the number of times the input has (excluding spec))
     cell = 0
-    for count in range(inventory_size):
-        # if remaining item exists print
-        if cell < inventory_size - 1:
-            if count < inventory_size - 1:
-                # print ever item in inventory_data from beinnign to end, max is from inventory_specs
-                if inventory_data[cell] > inventory_specs[cell]:
-                    print(f"({cell},{inventory_specs[cell]})", end="")
-                    inventory_data[cell] -= inventory_specs[cell]
-                else:
-                    print(f"({cell},{inventory_data[cell]})", end="")
-                    inventory_data[cell] = 0
-                cell += 1
-        # no items remaining: print -
+    for count in range(inventory_size): # loop the number of cells from input
+        item_id = id_list[cell]
+
+
+        # if items remaining exists print
+        if cell < inventory_size:   # id_list has not been exhausted
+
+            # print ever item in inventory_data from beinnign to end, max is from inventory_specs
+            if inventory_data[item_id] > inventory_specs[item_id]:
+                print(f"({item_id},{inventory_specs[item_id]})", end="")
+                inventory_data[item_id] -= inventory_specs[item_id]
+
+            # remaining items are less than inventory max
+            else:
+                print(f"({item_id},{inventory_data[item_id]})", end="")
+                inventory_data[item_id] = 0
+
+            cell += 1
+            # no items remaining: print -
         else:
-            print(f"({cell},{inventory_data[cell]})", end="")
+            print("-", end="")
         
 
         if (count+1) % inventory_col == 0:
---
+++ assignments/inventory/inventory_test.py
@@ -0,0 +1,96 @@
+def partition(arr, low, high):
+    pivot = arr[high][0]
+    i = low -1
+    for j in range(low, high):
+        if arr[j][0] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i + 1
+
+def quickSort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+
+        quickSort(arr, low, pi - 1)
+        quickSort(arr, pi + 1, high)
+    
+
+
+if __name__ == "__main__":
+
+    # save inventory id and stack size
+    temp_inventory = [] 
+    inventory_specs = {}    # data base for max items in cell
+    inventory_data = {} # data base for total number of items
+    inventory_col = 0   # how long single line is
+    inventory_size = 0  # how many cells are in input (preserve for output)
+
+
+
+    # input for inventory_specs
+    specs_input = input().strip()
+    specs_input = specs_input[1:-1]
+    specs = specs_input.split("),(")
+
+    for set in specs:
+        key, value = set.split(",")
+        temp_inventory.append((int(key), int(value)))
+
+    # Apply quickSort to dict
+    inventory_size = len(temp_inventory)
+    quickSort(temp_inventory, 0, inventory_size - 1)
+    
+    # convert back to dictionary
+    inventory_specs = dict(temp_inventory)
+    inventory_data = {key: 0 for key in inventory_specs.keys()}
+
+    print(inventory_specs)
+    print("-"*20)
+    # print(inventory_data)
+
+    
+    inventory_size = 0
+    # rest of input
+    while True:
+        try:
+            # remove all ( , ) from line
+            line = input().strip()
+
+            if not line:
+                break
+            
+            cells = line.replace("-", "(e,e)").replace('),(', ')#(').split('#')
+            # (e, e) fake cell for easier format
+
+            for item in cells:
+                
+                item = item.strip(",")
+                # print(item)
+                
+                # item is "(a,b)"
+                item = item.strip("()")
+                item_id, item_count = item.split(",")
+
+                # empty cell
+                if item_id == "e":
+                    #print("empty cell")
+                    continue
+
+                inventory_data[int(item_id)] += int(item_count)
+                # print(f"{item_count} added to {item_id}")
+
+            # (1,5),(2,10),(3,9)
+            # -,(3,2),(1,5)
+            
+            if inventory_col != 0 and inventory_col != len(cells):
+                print("error in col size")
+                break
+            inventory_col = len(cells)         # e.g. columns per row
+            inventory_size += inventory_col
+
+        except EOFError:
+            break
+
+    print(inventory_size)
+    print(inventory_data)
